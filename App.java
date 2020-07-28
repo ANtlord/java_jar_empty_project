8540b625836fc85157e7b4c106e7cf456db2c0e6
ANtlord <antlord@localhost.localdomain>
2020-07-28 14:53:32 +0300

    init commit

---
+++ App.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF; // go to definition doesn't work
+
+class App {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        WeightedQuickUnionUF instance = new WeightedQuickUnionUF(5);
+        instance. // completion doesn't work
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Sample project for the [issue](https://github.com/eclipse/eclipse.jdt.ls/issues/1457#issuecomment-664722756)
+
+Run `CLASSPATH="./lib/*" javac App.java` to compile it
